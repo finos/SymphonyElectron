207d42aa5ed2b9ff85c3ab8998f94684e50f79a7
Lynn <lynn@symphony.com>
2017-05-23 13:51:19 -0700

    electron-37: added implementation for advanced mac installer (#99)

---
+++ installer/mac/SymphonySettingsPlugin/SymphonySettingsPlugin/MyInstallerPane.h
@@ -0,0 +1,15 @@
+//
+//  MyInstallerPane.h
+//  SymphonySettingsPlugin
+//
+//  Copyright © 2017 Symphony. All rights reserved.
+//
+
+#import <InstallerPlugins/InstallerPlugins.h>
+
+@interface MyInstallerPane : InstallerPane
+
+@property (weak) IBOutlet NSButton *minimizeOnCloseCheckBox;
+@property (weak) IBOutlet NSButton *autoLaunchCheckBox;
+
+@end
