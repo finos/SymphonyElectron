fbfe89599c7b0b2e938e43e62f887635fe530cc9
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2017-05-26 18:35:52 +0530

    electron-37: fixed issues w.r.t. installer pane not capturing the pod url and also added startup plist in the post install script

--- js/main.js
+++ js/main.js
@@ -141,9 +141,9 @@ function getUrlAndCreateMainWindow() {
 
     getConfigField('url')
         .then(createWin).catch(function (err) {
-        let title = 'Error loading configuration';
-        electron.dialog.showErrorBox(title, title + ': ' + err);
-    });
+            let title = 'Error loading configuration';
+            electron.dialog.showErrorBox(title, title + ': ' + err);
+        });
 }
 
 function createWin(urlFromConfig) {
