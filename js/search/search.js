0f0310f07e66d9026b1f6d51b8f8c6342f8f78f0
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-08-03 14:31:59 +0530

    indexvalidator

--- js/search/search.js
+++ js/search/search.js
@@ -27,7 +27,8 @@ const MINIMUM_DATE = '0000000000000';
 const MAXIMUM_DATE = '9999999999999';
 const INDEX_VERSION = 'v1';
 
-const rootPath = isMac ? 'indexvalidator.exec' : (process.arch === 'ia32'? 'indexvalidator-x86.exe' : 'indexvalidator-x64.exe');
+const winArchPath = process.arch === 'ia32' ? 'indexvalidator-x86.exe' : 'indexvalidator-x64.exe';
+const rootPath = isMac ? 'indexvalidator.exec' : winArchPath;
 const productionPath = path.join(execPath, isMac ? '..' : '', rootPath);
 const devPath = path.join(__dirname, '..', '..', rootPath);
 const libraryPath = isDevEnv ? devPath : productionPath;
