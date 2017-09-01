0b003d1df9c4f33658fde49507be5d28ac3daf8a
Kiran Niranjan <kiran@kike.co.in>
2017-09-01 21:35:38 +0530

    Electron-125 - Flipped the condition

--- js/desktopCapturer/getSources.js
+++ js/desktopCapturer/getSources.js
@@ -45,7 +45,7 @@ function getSources(options, callback) {
     captureScreen = includes.call(options.types, 'screen');
 
     let updatedOptions = options;
-    if (updatedOptions.thumbnailSize == null) {
+    if (updatedOptions.thumbnailSize !== null) {
         updatedOptions.thumbnailSize = {
             width: 150,
             height: 150
