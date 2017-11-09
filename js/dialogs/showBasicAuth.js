0b7bad60e319bab23863191217b1cabc2afeb728
Kiran Niranjan <kiran@kike.co.in>
2017-11-09 17:31:38 +0530

    Electron-206 - Implemented basic auth error handler

--- js/dialogs/showBasicAuth.js
+++ js/dialogs/showBasicAuth.js
@@ -3,6 +3,8 @@
 const electron = require('electron');
 
 const basicAuth = require('../basicAuth');
+let currentAuthURL;
+let tries = 0;
 
 /**
  * Having a proxy or hosts that requires authentication will allow user to
@@ -12,14 +14,30 @@ electron.app.on('login', (event, webContents, request, authInfo, callback) => {
 
     event.preventDefault();
 
+    if(currentAuthURL !== request.url) {
+        currentAuthURL = request.url;
+    } else {
+        tries++
+    }
+
     // name of the host to display
     let hostname = authInfo.host || authInfo.realm;
     let browserWin = electron.BrowserWindow.fromWebContents(webContents);
     let windowName = browserWin.winName || '';
 
     /**
+     * Method that resets currentAuthURL and tries
+     * if user closes the auth window
+     */
+    function clearSettings() {
+        callback();
+        currentAuthURL = '';
+        tries = 0;
+    }
+
+    /**
      * Opens an electron modal window in which
      * user can enter credentials fot the host
      */
-    basicAuth.openBasicAuthWindow(windowName, hostname, callback);
+    basicAuth.openBasicAuthWindow(windowName, hostname, tries === 0, clearSettings, callback);
 });
