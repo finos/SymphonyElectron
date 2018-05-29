da7da855a9f9101e96daf2da481e33325a1085a9
Daniel Gronberg <daniel.gronberg@symphony.com>
2018-05-29 14:44:59 +0200

    Non cases sensitive cmd line args

--- js/utils/getCmdLineArg.js
+++ js/utils/getCmdLineArg.js
@@ -4,7 +4,7 @@ const log = require('../log.js');
 const logLevels = require('../enums/logLevels.js');
 
 /**
- * Search given argv for argName using exact match or starts with.
+ * Search given argv for argName using exact match or starts with. Comparison is case insensitive
  * @param  {Array} argv       Array of strings
  * @param  {String} argName   Arg name to search for.
  * @param  {Boolean} exactMatch  If true then look for exact match otherwise
@@ -17,9 +17,11 @@ function getCmdLineArg(argv, argName, exactMatch) {
         return null;
     }
 
+    let argNameToFind = argName.toLocaleLowerCase();
     for (let i = 0, len = argv.length; i < len; i++) {
-        if ((exactMatch && argv[i] === argName) ||
-            (!exactMatch && argv[i].startsWith(argName))) {
+        let arg = argv[i].toLocaleLowerCase();
+        if ((exactMatch && arg === argNameToFind) ||
+            (!exactMatch && arg.startsWith(argNameToFind))) {
             return argv[i];
         }
     }
