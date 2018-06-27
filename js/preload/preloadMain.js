4aaa9ac5393aa8eade31c7b3b806d123696d5943
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2018-06-27 14:12:16 +0530

    Swift Search to Master (#405)
    * SEARCH-627 & SEARCH-628
    
    - Add implementation for memory indexing
    - Batch indexing and Real-time indexing
    
    * ELECTRON-426 (#345)
    
    - Changes innerHTML to innerText
    - Removes 'replaceStrongTag' and 'replaceHTMLTags' function and changing to innerHTML to innerText
    
    * SEARCH-764
    
    - Rework "deleteIndexFolders" in the Electron Preload Script
    - Fixes the Security Vulnerability
    
    * SEARCH-764
    
    - Updates spectron test preload api version
    
    * SEARCH-766
    
    - Rework "readFile" in Electron Preload Script
    - Removing this method as this is only for the demo app
    
    * SEARCH-766
    
    - Removes merge method
    
    * SEARCH-767
    
    - Rework "path" in Electron Preload Script
    - Removes the constructor to use hardcoded path
    
    * SEARCH-767
    
    - Updates unit test
    
    * SEARCH-775
    
    - Adds search api version to match with the client app
    
    * SEARCH-775
    
    - Spectron test fix
    
    * SEARCH-770 (#391)
    
    - constructor is required which was removed as part of SEARCH-767

--- js/preload/preloadMain.js
+++ js/preload/preloadMain.js
@@ -110,7 +110,8 @@ function createAPI() {
                     containerIdentifier: appName,
                     containerVer: appVer,
                     buildNumber: buildNumber,
-                    apiVer: '1.0.0'
+                    apiVer: '2.0.0',
+                    searchApiVer: '2.0.0'
                 };
                 resolve(verInfo);
             });
