8c3da3e505e4c8b1cfe1446e576561f2c7ce3ec0
Thai Pham <pham.thai@symphony.com>
2018-07-11 16:02:35 +0700

    Updated

--- tests/spectron/spectronConstants.js
+++ tests/spectron/spectronConstants.js
@@ -9,7 +9,7 @@ module.exports = {
     SEARCH_LIBRARY_PATH_WIN: "node_modules/electron/dist/library",
 
     TESTED_HOST: "https://cip4-qa.symphony.com/",
-    ADMIN_USERNAME: "qa_auto_admin",
+    ADMIN_USERNAME: "spectron_auto_admin",
     ADMIN_PASSWORD: "Symphony!123456",
 };
 
