f893b5658cc1728ddca65ed42c462f9f0ad7c993
tranducanh <anh.duc@symphony.com>
2018-08-01 16:13:57 +0700

    AVT-937,AVT-938,AVT-939 : [Spectron][Windows] Close window when 'Minimize on Close' is ON (#437)

--- tests/spectron/spectronInterfaces.js
+++ tests/spectron/spectronInterfaces.js
@@ -1,8 +1,9 @@
-module.exports= {
-    
+module.exports= {    
     // Title bar
     TITLE_BAR: "#title-bar",
-    MAXIMIZE_BTN: "#title-bar-maximize-button",
-    
-    SYM_LOGO: "#logo"
+    MAXIMIZE_BTN: "#title-bar-maximize-button",    
+    SYM_LOGO: "#logo",
+    MINIMIZED_BUTTON: "button#title-bar-minimize-button",
+    CLOSE_BUTTON: "button#title-bar-close-button",
+    MAIN_MENU_ITEM: "#hamburger-menu-button"
 };
--- tests/spectron/spectronWebActions.js
+++ tests/spectron/spectronWebActions.js
@@ -3,8 +3,8 @@ const ui = require('./spectronInterfaces.js');
 class WebActions {
     constructor(app) {
             this.app = app;
-    }
-    
+    }    
+
     async clickMaximizeButton(){
         await this.app.client.waitForVisible(ui.MAXIMIZE_BTN, 10000).click(ui.MAXIMIZE_BTN);
     }
@@ -15,6 +15,19 @@ class WebActions {
             expect(maximized).toBeTruthy();
         })
     }
+
+    async minimizeWindowByClick() {       
+        await this.app.client.click(ui.MINIMIZED_BUTTON);
+    }
+
+    async closeWindowByClick() {        
+        await this.app.client.click(ui.CLOSE_BUTTON);
+    }
+
+    async openApplicationMenuByClick() {       
+        await this.app.client.click(ui.MAIN_MENU_ITEM);
+    }
+
 }
 
 module.exports = WebActions;
