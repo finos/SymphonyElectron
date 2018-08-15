e1b978c48a7ffb9bcbabf443350577afec2afdeb
Tran Duc Anh <anh.duc@symphony.com>
2018-08-15 08:54:18 +0700

    remove check isMac

--- tests/spectron/toastFlashWhenOpenMultiApplication.spectron.js
+++ tests/spectron/toastFlashWhenOpenMultiApplication.spectron.js
@@ -2,8 +2,8 @@ const Application = require('./spectronSetup');
 const WebDriver = require('./spectronWebDriver');
 const { isMac } = require('../../js/utils/misc.js');
 var app = new Application({
-  startTimeout: Application.getTimeOut(),
-  waitTimeout: Application.getTimeOut()
+    startTimeout: Application.getTimeOut(),
+    waitTimeout: Application.getTimeOut()
 });
 var webdriver = new WebDriver({ browser: 'chrome' });
 const WindowsAction = require('./spectronWindowsActions');
@@ -16,15 +16,14 @@ let webActions, windowAction;
 !isMac ? describe('Verify Flash notification in taskbar option when multiple applications are opened', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = Application.getTimeOut();
     let originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    beforeAll(async(done) => {
-        try
-        {
-          app = await new Application({}).startApplication({testedHost:specconst.TESTED_HOST, alwaysOnTop: true});
-            windowAction = await new WindowsAction(app);           
-            webActions = await new WebActions(app); 
-            webdriver.webAction = webActions;      
+    beforeAll(async (done) => {
+        try {
+            app = await new Application({}).startApplication({ testedHost: specconst.TESTED_HOST, alwaysOnTop: true });
+            windowAction = await new WindowsAction(app);
+            webActions = await new WebActions(app);
+            webdriver.webAction = webActions;
             done();
-        } catch(err) {
+        } catch (err) {
             done.fail(new Error(`Unable to start application error: ${err}`));
         };
     });
@@ -32,42 +31,37 @@ let webActions, windowAction;
         if (app && app.isRunning()) {
             jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
             app.stop().then(() => {
-              webdriver.close();
-              webdriver.quit();
-              done();
+                webdriver.close();
+                webdriver.quit();
+                done();
             }).catch((err) => {
-              done();
+                done();
             });
         }
     });
-   /**
-     * Verify Flash notification in taskbar option when multiple applications are opened
-     * TC-ID: 47308146
-    * Cover scenarios in AVT-1083
-    */
+    /**
+      * Verify Flash notification in taskbar option when multiple applications are opened
+      * TC-ID: 47308146
+     * Cover scenarios in AVT-1083
+     */
     it('Verify Flash notification in taskbar option when multiple applications are opened', async () => {
-        if (isMac)
-        {
-            done();
-        }
-        else
-        {
-            await webdriver.startDriver();
-            await webdriver.login(specconst.USER_A);
-            await webdriver.createIM(specconst.USER_B.username);
-            await webActions.login(specconst.USER_B);
-            await windowAction.reload();    
-            await app.client.waitForVisible(ifc.SETTTING_BUTTON, Utils.toMs(50));
-            await webActions.clickIfElementVisible(ifc.SETTTING_BUTTON);
-            await windowAction.pressCtrlM();    
-            await webdriver.clickLeftNavItem(specconst.USER_B.name);
-            let messages = [];
-            await messages.push(await Utils.randomString());
-            await messages.push(await Utils.randomString());
-            await messages.push(await Utils.randomString());     
-            await webdriver.sendMessagesAndVerifyToast(messages); 
-        }
+
+        await webdriver.startDriver();
+        await webdriver.login(specconst.USER_A);
+        await webdriver.createIM(specconst.USER_B.username);
+        await webActions.login(specconst.USER_B);
+        await windowAction.reload();
+        await app.client.waitForVisible(ifc.SETTTING_BUTTON, Utils.toMs(50));
+        await webActions.clickIfElementVisible(ifc.SETTTING_BUTTON);
+        await windowAction.pressCtrlM();
+        await webdriver.clickLeftNavItem(specconst.USER_B.name);
+        let messages = [];
+        await messages.push(await Utils.randomString());
+        await messages.push(await Utils.randomString());
+        await messages.push(await Utils.randomString());
+        await webdriver.sendMessagesAndVerifyToast(messages);
+
     });
-  
-}): describe.skip();
+
+}) : describe.skip();
 
