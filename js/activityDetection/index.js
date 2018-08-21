49e21c699e884dc6becd81db054b61d5dcba8b0e
Kiran Niranjan <kiran.niranjan@symphony.com>
2018-08-21 11:59:57 +0530

    ELECTRON-694 - Fix require statement for ELECTRON_QA env

--- js/activityDetection/index.js
+++ js/activityDetection/index.js
@@ -4,12 +4,14 @@ const systemIdleTime = require('@paulcbetts/system-idle-time');
 const throttle = require('../utils/throttle');
 const log = require('../log.js');
 const logLevels = require('../enums/logLevels.js');
-const { getIsOnline } = require('../windowMgr');
 
-let setIsAutoReload;
+let setIsAutoReloadFnc;
+let getIsOnlineFnc;
 if (!process.env.ELECTRON_QA) {
     /* eslint-disable global-require */
-    setIsAutoReload = require('../windowMgr').setIsAutoReload;
+    const { getIsOnline, setIsAutoReload } = require('../windowMgr');
+    getIsOnlineFnc = getIsOnline;
+    setIsAutoReloadFnc = setIsAutoReload;
     /* eslint-enable global-require */
 }
 
@@ -56,11 +58,11 @@ function monitorUserActivity() {
     intervalId = setInterval(monitor, 1000);
 
     function monitor() {
-        if (systemIdleTime.getIdleTime() < maxIdleTime && getIsOnline()) {
+        if (systemIdleTime.getIdleTime() < maxIdleTime && typeof getIsOnlineFnc === 'function' && getIsOnlineFnc()) {
             // If system is active, send an update to the app bridge and clear the timer
             sendActivity();
-            if (typeof setIsAutoReload === 'function') {
-                setIsAutoReload(false);
+            if (typeof setIsAutoReloadFnc === 'function') {
+                setIsAutoReloadFnc(false);
             }
             clearInterval(intervalId);
             intervalId = undefined;
