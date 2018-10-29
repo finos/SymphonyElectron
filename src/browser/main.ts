3f15dc213d65a642b51caf532175850af8ee98d7
Kiran Niranjan <kiran.niranjan@symphony.com>
2018-10-30 00:22:58 +0530

    Typescript: Completed about app

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -8,7 +8,6 @@ import { autoLaunchInstance } from './auto-launch-controller';
 import setChromeFlags from './chrome-flags';
 import { config } from './config-handler';
 import { windowHandler } from './window-handler';
-import { createComponentWindow } from './window-utils';
 
 const allowMultiInstance: string | boolean = getCommandLineArgs(process.argv, '--multiInstance', true) || isDevEnv;
 const singleInstanceLock: boolean = allowMultiInstance ? true : app.requestSingleInstanceLock();
@@ -37,8 +36,6 @@ async function main() {
          */
         await autoLaunchInstance.handleAutoLaunch();
     }
-
-    createComponentWindow('about-app');
 
     /**
      * Sets chrome flags from global config
