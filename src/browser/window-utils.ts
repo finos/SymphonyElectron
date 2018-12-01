f886dd937e113d9767d2d480d0ba5afabc555d30
Kiran Niranjan <kiran.niranjan@symphony.com>
2018-12-01 15:16:55 +0530

    Typescript - Add set badge count api

--- src/browser/window-utils.ts
+++ src/browser/window-utils.ts
@@ -1,6 +1,9 @@
-import { BrowserWindow } from 'electron';
+import { app, BrowserWindow, nativeImage } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
+
+import { isMac } from '../common/env';
+import { logger } from '../common/logger';
 import { windowHandler } from './window-handler';
 
 /**
@@ -58,4 +61,49 @@ export function preventWindowNavigation(browserWindow: Electron.BrowserWindow) {
     };
 
     browserWindow.webContents.on('will-navigate', listener);
+}
+
+/**
+ * Shows the badge count
+ * @param count {number}
+ */
+export function showBadgeCount(count: number): void {
+    if (typeof count !== 'number') {
+        logger.warn(`badgeCount: invalid func arg, must be a number: ${count}`);
+        return;
+    }
+
+    if (isMac) {
+        // too big of a number here and setBadgeCount crashes
+        app.setBadgeCount(Math.min(1e8, count));
+        return;
+    }
+
+    // handle ms windows...
+    const mainWindow = windowHandler.getMainWindow();
+    if (mainWindow) {
+        if (count > 0) {
+            // get badge img from renderer process, will return
+            // img dataUrl in setDataUrl func.
+            mainWindow.webContents.send('create-badge-data-url', { count });
+        } else {
+            // clear badge count icon
+            mainWindow.setOverlayIcon(null, '');
+        }
+    }
+}
+
+/**
+ * Sets the data url
+ * @param dataUrl
+ * @param count
+ */
+export function setDataUrl(dataUrl: string, count: number): void {
+    const mainWindow = windowHandler.getMainWindow();
+    if (mainWindow && dataUrl && count) {
+        const img = nativeImage.createFromDataURL(dataUrl);
+        // for accessibility screen readers
+        const desc = 'Symphony has ' + count + ' unread messages';
+        mainWindow.setOverlayIcon(img, desc);
+    }
 }
--- src/renderer/preload-main.ts
+++ src/renderer/preload-main.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import WindowsTitleBar from '../renderer/windows-title-bar';
+import { SSFApi } from './ssf-api';
 
 document.addEventListener('DOMContentLoaded', load);
 
@@ -11,4 +12,27 @@ document.addEventListener('DOMContentLoaded', load);
 function load() {
     const element = React.createElement(WindowsTitleBar);
     ReactDOM.render(element, document.body);
+}
+
+createAPI();
+
+/**
+ * creates API exposed from electron.
+ */
+function createAPI() {
+    // iframes (and any other non-top level frames) get no api access
+    // http://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t/326076
+    if (window.self !== window.top) {
+        return;
+    }
+
+    // note: window.open from main window (if in the same domain) will get
+    // api access.  window.open in another domain will be opened in the default
+    // browser (see: handler for event 'new-window' in windowMgr.js)
+
+    //
+    // API exposed to renderer process.
+    //
+    // @ts-ignore
+    window.ssf = new SSFApi();
 }
