afffd054e4b48b44da38d2b6cf6450a0e244cc0f
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-01-08 12:20:50 +0530

    Typescript - Add gulp tasks to compile less, typescript and copy files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
+const tsc = require('gulp-tsc');
+const del = require('del');
 const path = require('path');
+
+// TODO: Add gulp watch tasks
+
+gulp.task('clean', function() {
+    return del('lib');
+});
+
+gulp.task('compile', function() {
+    return gulp.src(['src/**/*.ts'])
+        .pipe(tsc({ project: './tsconfig.json' }))
+        .pipe(gulp.dest('lib/'))
+});
 
 gulp.task('less', function () {
     return gulp.src('./src/**/*.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.join(__dirname, 'src')));
+        .pipe(gulp.dest(path.join(__dirname, 'lib/src')));
 });
+
+gulp.task('copy', function () {
+    return gulp.src([
+        './src/renderer/assets/*',
+        './src/renderer/*.html'
+    ], {
+        "base": "./src"
+    }).pipe(gulp.dest('lib/src'))
+});
+
+gulp.task('build', gulp.series('clean', 'compile', 'less', 'copy'));
