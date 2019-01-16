c91f624249f7cf76719036663f8f9f812a3fc80d
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-01-16 22:50:47 +0530

    Typescript - handle multiple/single instance of the client and add safety checks

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -1,6 +1,6 @@
 import { app } from 'electron';
 
-import { isDevEnv } from '../common/env';
+import { isDevEnv, isMac } from '../common/env';
 import { logger } from '../common/logger';
 import { getCommandLineArgs } from '../common/utils';
 import { cleanUpAppCache, createAppCacheFile } from './app-cache-handler';
@@ -10,15 +10,14 @@ import { config } from './config-handler';
 import './dialog-handler';
 import './main-api-handler';
 import { SpellChecker } from './spell-checker-handler';
-import { windowHandler } from './window-handler';
+import { ICustomBrowserWindow, windowHandler } from './window-handler';
 
 const allowMultiInstance: string | boolean = getCommandLineArgs(process.argv, '--multiInstance', true) || isDevEnv;
-const singleInstanceLock: boolean = allowMultiInstance ? true : app.requestSingleInstanceLock();
 
 /**
  * Main function that init the application
  */
-const main = async () => {
+const startApplication = async () => {
     await app.whenReady();
     const spellchecker = new SpellChecker();
     logger.info(`initialized spellchecker module with locale ${spellchecker.locale}`);
@@ -42,10 +41,34 @@ const main = async () => {
     setChromeFlags();
 };
 
-if (!singleInstanceLock) {
-    app.quit();
+// Handle multiple/single instances
+if (!allowMultiInstance) {
+    logger.info('Multiple instance not allowed requesting lock', { allowMultiInstance });
+    const gotTheLock = app.requestSingleInstanceLock();
+
+    // quit if another instance is already running, ignore for dev env or if app was started with multiInstance flag
+    if (!gotTheLock) {
+        logger.info('Got the lock hence closing the instance', { gotTheLock });
+        app.quit();
+    } else {
+        logger.info('Creating the first instance of the application');
+        app.on('second-instance', (_event) => {
+            // Someone tried to run a second instance, we should focus our window.
+            const mainWindow = windowHandler.getMainWindow();
+            if (mainWindow && !mainWindow.isDestroyed()) {
+                if (isMac) return mainWindow.show();
+                if (mainWindow.isMinimized()) {
+                    mainWindow.restore();
+                }
+                mainWindow.focus();
+                // TODO: Handle protocol action
+            }
+        });
+        startApplication();
+    }
 } else {
-    main();
+    logger.info('Multiple instance allowed hence create application', { allowMultiInstance });
+    startApplication();
 }
 
 /**
@@ -58,3 +81,17 @@ app.on('window-all-closed', () => app.quit());
  * Creates a new empty cache file when the app is quit
  */
 app.on('quit',  () => cleanUpAppCache());
+
+/**
+ * Cleans up reference before quiting
+ */
+app.on('before-quit', () => windowHandler.willQuitApp = true);
+
+app.on('activate', () => {
+    const mainWindow: ICustomBrowserWindow | null = windowHandler.getMainWindow();
+    if (!mainWindow || mainWindow.isDestroyed()) {
+        startApplication();
+    } else {
+        mainWindow.show();
+    }
+});
