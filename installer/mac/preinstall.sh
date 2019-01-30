c7a780a8279892e3076081e2243c2061e633d248
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2019-01-30 05:51:34 +0530

    update version number in preinstall script

--- installer/mac/preinstall.sh
+++ installer/mac/preinstall.sh
@@ -12,7 +12,7 @@ delete_app()
 compare_versions()
 {
     # Get the installer version:
-    CURRENT_VERSION=4.3.1
+    CURRENT_VERSION=4.3.3
 
     # Get the currently installed version:
     INSTALLED_VERSION=$(plutil -p /Applications/Symphony.app/Contents/Info.plist | awk '/CFBundleShortVersionString/ {print substr($3, 2, length($3)-2)}')
