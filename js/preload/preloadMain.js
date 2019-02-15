543a6af5eae4f8cdea9e7f1b1803848df944e630
Victor R. Braga de Sales Mascarenhasm <vmascarenhas@daitangroup.com>
2019-02-15 17:37:08 -0200

    ELECTRON-1019: enable spellchecker for popup window

--- js/preload/preloadMain.js
+++ js/preload/preloadMain.js
@@ -86,10 +86,6 @@ const throttledSetIsInMeetingStatus = throttle(1000, function (isInMeeting) {
 local.ipcRenderer.on('on-page-load', () => {
     snackBar = new SnackBar();
 
-    // Enable spellchecker only for main window until
-    // the underline memory leak is fixed on electron
-    // https://github.com/electron/electron/issues/15459
-    if (window.name === 'main') {
         webFrame.setSpellCheckProvider('en-US', true, {
             spellCheck (text) {
                 return !local.ipcRenderer.sendSync(apiName, {
@@ -98,7 +94,6 @@ local.ipcRenderer.on('on-page-load', () => {
                 });
             }
         });
-    }
 
     // only registers main window's preload
     if (window.name === 'main') {
