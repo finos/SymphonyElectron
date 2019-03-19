fba2f6bb965de1585f60afa2b2576316b87312ef
Victor R. Braga de Sales Mascarenhasm <vmascarenhas@daitangroup.com>
2019-03-19 16:03:39 -0300

    app cache handler unit test

---
+++ spec/appCacheHandler.spec.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { cleanUpAppCache, createAppCacheFile } from '../src/app/app-cache-handler';
+import { app, session } from './__mocks__/electron';
+
+jest.mock('fs', () => ({
+        writeFileSync: jest.fn(),
+        existsSync: jest.fn(() => true),
+        unlinkSync: jest.fn(),
+}));
+
+describe('app cache handler', () => {
+    const cachePathExpected = path.join(app.getPath('userData'), 'CacheCheck');
+
+    it('should call `cleanUpAppCache` correctly', () => {
+        const spyFn = 'unlinkSync';
+        const spy = jest.spyOn(fs, spyFn);
+        cleanUpAppCache();
+        expect(spy).toBeCalledWith(cachePathExpected);
+    });
+
+    it('should call `clearCache` when `session.defaultSession` is not null', () => {
+        jest.spyOn(fs, 'existsSync').mockImplementation(() => false);
+        const spyFn = 'clearCache';
+        const spy = jest.spyOn(session.defaultSession, spyFn);
+        cleanUpAppCache();
+        expect(spy).lastCalledWith(expect.any(Function));
+    });
+
+    it('should call `createAppCacheFile` correctly', () => {
+        const spyFn = 'writeFileSync';
+        const spy = jest.spyOn(fs, spyFn);
+        createAppCacheFile();
+        expect(spy).lastCalledWith(cachePathExpected, '');
+    });
+});
