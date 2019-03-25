a223003186685ff62430456a87e5caa6712eea0f
Victor R. Braga de Sales Mascarenhas <vmascarenhas@daitangroup.com>
2019-03-25 17:09:35 -0300

    reverting screen snippet: change extension from png to jpeg on windows

--- js/screenSnippet/index.js
+++ js/screenSnippet/index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-const { isMac, isDevEnv, isWindowsOS } = require('../utils/misc.js');
+const { isMac, isDevEnv } = require('../utils/misc.js');
 const log = require('../log.js');
 const logLevels = require('../enums/logLevels.js');
 const eventEmitter = require('.././eventEmitter');
@@ -41,7 +41,7 @@ class ScreenSnippet {
 
             log.send(logLevels.INFO, 'ScreenSnippet: starting screen capture');
 
-            let tmpFilename = (isWindowsOS) ? 'symphonyImage-' + Date.now() + '.png' : 'symphonyImage-' + Date.now() + '.jpg';
+            let tmpFilename ='symphonyImage-' + Date.now() + '.jpg';
             let tmpDir = os.tmpdir();
 
             let outputFileName = path.join(tmpDir, tmpFilename);
@@ -145,7 +145,7 @@ function readResult(outputFileName, resolve, reject, childProcessErr) {
             // convert binary data to base64 encoded string
             let output = Buffer.from(data).toString('base64');
             const resultOutput = {
-                type: isWindowsOS ? 'image/png;base64' : 'image/jpg;base64',
+                type: 'image/jpg;base64',
                 data: output
             };
             resolve(resultOutput);
