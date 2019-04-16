c026a1ef1e8231fcf3119c78bb68964c907bf3a9
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-04-16 11:03:52 +0530

    Typescript - Add backward compatibility support for screen snippet

---
+++ src/renderer/screen-snippet-bc-handler.ts
@@ -0,0 +1,27 @@
+import { ipcRenderer } from 'electron';
+import { apiCmds, apiName, IScreenSnippet } from '../common/api-interface';
+
+/**
+ * @deprecated user openScreenSnippet instead
+ */
+export class ScreenSnippetBcHandler {
+    /**
+     * capture method to support backward compatibility
+     *
+     * @deprecated user openScreenSnippet instead
+     */
+    public capture(): Promise<IScreenSnippet> {
+        return new Promise((resolve, reject) => {
+            ipcRenderer.send(apiName.symphonyApi, {
+                cmd: apiCmds.openScreenSnippet,
+            });
+            ipcRenderer.on('screen-snippet-data', (_event: Event, arg: IScreenSnippet) => {
+                if (arg.type === 'ERROR') {
+                    reject(arg);
+                    return;
+                }
+                resolve(arg);
+            });
+        });
+    }
+}
