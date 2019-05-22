b2cffe371429c3e457dfaf6b8fea8b271d3d0f30
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-05-22 14:13:45 +0530

    Set shell path

--- src/app/init.ts
+++ src/app/init.ts
@@ -1,7 +1,8 @@
 import { app } from 'electron';
 import * as path from 'path';
+import * as shellPath from 'shell-path';
 
-import { isDevEnv } from '../common/env';
+import { isDevEnv, isMac } from '../common/env';
 import { getCommandLineArgs } from '../common/utils';
 
 // Handle custom user data path from process.argv
@@ -17,6 +18,31 @@ if (isDevEnv) {
 if (userDataPath) {
     app.setPath('userData', userDataPath);
 }
+
+// Setting the env path child_process issue https://github.com/electron/electron/issues/7688
+(async () => {
+    try {
+        const paths = await shellPath();
+        if (paths) {
+            return process.env.PATH = paths;
+        }
+        if (isMac) {
+            process.env.PATH = [
+                './node_modules/.bin',
+                '/usr/local/bin',
+                process.env.PATH,
+            ].join(':');
+        }
+    } catch (e) {
+        if (isMac) {
+            process.env.PATH = [
+                './node_modules/.bin',
+                '/usr/local/bin',
+                process.env.PATH,
+            ].join(':');
+        }
+    }
+})();
 
 // tslint:disable-next-line
 require('./main');
