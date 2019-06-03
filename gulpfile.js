019956fa8082d1f1f8b9bc00de1cba8d3682ba45
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2019-06-03 13:40:23 +0530

    ELECTRON-1204: implement ttl logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
@@ -11,6 +12,10 @@ gulp.task('clean', function() {
     return del('lib');
 });
 
+/**
+ * Compile all typescript files
+ * and copy to the destination
+ */
 gulp.task('compile', function() {
     return gulp.src(['src/**/*.ts', 'src/**/*.tsx'])
         .pipe(tsc({ project: './tsconfig.json' }))
@@ -25,6 +30,9 @@ gulp.task('less', function () {
         .pipe(gulp.dest(path.join(__dirname, 'lib/src')));
 });
 
+/**
+ * Copy all assets to JS codebase
+ */
 gulp.task('copy', function () {
     return gulp.src([
         './src/renderer/assets/*',
@@ -33,6 +41,35 @@ gulp.task('copy', function () {
     ], {
         "base": "./src"
     }).pipe(gulp.dest('lib/src'))
+});
+
+/**
+ * Set expiry time for test builds
+ */
+gulp.task('setExpiry', function (done) {
+    // Set expiry of 15 days for test builds we create from CI
+    const expiryDays = 15;
+    const milliseconds = 24*60*60*1000;
+    const expiryTime = new Date().getTime() + (expiryDays * milliseconds);
+
+    const ttlHandlerFile = path.join(__dirname, 'src/app/ttl-handler.ts');
+    fs.readFile(ttlHandlerFile, 'utf8', function (err,data) {
+        if (err) {
+            console.log(err);
+            return done(err);
+        }
+
+        // Do a simple search and replace in the `ttl-handler.ts` file
+        const replacementString = `const ttlExpiryTime = ${expiryTime}`;
+        var result = data.replace(/const ttlExpiryTime = -1/g, replacementString);
+
+        fs.writeFile(ttlHandlerFile, result, 'utf8', function (err) {
+            if (err) {
+                return done(err);
+            }
+            done();
+        });
+    });
 });
 
 gulp.task('build', gulp.series('clean', 'compile', 'less', 'copy'));
---
+++ src/app/ttl-handler.ts
@@ -0,0 +1,28 @@
+import { logger } from '../common/logger';
+
+const ttlExpiryTime = -1;
+
+export const getExpiryTime = (): number => {
+    return ttlExpiryTime;
+};
+
+/**
+ * Checks to see if the build is expired against a TTL expiry time
+ */
+export const checkIfBuildExpired = (): boolean => {
+    logger.info(`ttl-handler: Checking for build expiry`);
+
+    const expiryTime = getExpiryTime();
+
+    if (expiryTime <= -1) {
+        logger.info(`ttl-handler: Expiry not applicable for this build`);
+        return false;
+    }
+
+    const currentDate: Date = new Date();
+    const expiryDate: Date = new Date(expiryTime);
+    logger.info(`ttl-handler: Current Time: ${currentDate.getTime()}, Expiry Time: ${expiryDate.getTime()}`);
+
+    const buildExpired = currentDate.getTime() > expiryDate.getTime();
+    return buildExpired;
+};
