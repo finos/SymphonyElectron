920ff9cdbafc55550e81f9cdc919d7467444ceee
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-08-30 14:46:32 +0530

    ELECTRON-1505 - Prevent opening links longer than 2083

--- src/app/child-window-handler.ts
+++ src/app/child-window-handler.ts
@@ -193,6 +193,11 @@ export const handleChildWindow = (webContents: WebContents): void => {
                 }
             });
         } else {
+            if (newWinUrl && newWinUrl.length > 2083) {
+                logger.info(`child-window-handler: new window url length is greater than 2083, not performing any action!`);
+                event.preventDefault();
+                return;
+            }
             logger.info(`child-window-handler: new window url is ${newWinUrl} which is not of the same host,
             so opening it in the default browser!`);
             event.preventDefault();
