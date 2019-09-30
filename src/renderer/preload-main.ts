2b7a0aa8aef23fe8c1448f561db913441c8d25ed
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2019-09-30 15:55:26 +0530

    SDA-1510: add sda title bar class

--- src/renderer/preload-main.ts
+++ src/renderer/preload-main.ts
@@ -85,6 +85,8 @@ ipcRenderer.on('page-load', (_event, { locale, resources, enableCustomTitleBar, 
         const div = document.createElement( 'div' );
         document.body.appendChild(div);
         ReactDOM.render(element, div);
+
+        document.body.classList.add('sda-title-bar');
     }
 
     webFrame.setSpellCheckProvider('en-US', {
