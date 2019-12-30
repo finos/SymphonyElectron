c3cff2e67ab6b4c5ed8570cf78de93ad7fa649a2
Vishwas Shashidhar <VishwasShashidhar@users.noreply.github.com>
2019-12-30 21:06:47 +0530

    chore: SDA-1635: add snyk to builds (#839)
    * SDA-1635: add support for running snyk tests on builds
    
    * SDA-1635: remove command checks for windows scripts
    
    * SDA-1635: fix windows batch scripts
    
    * SDA-1635: remove windows node version check

--- scripts/build-win32.bat
+++ scripts/build-win32.bat
@@ -6,6 +6,7 @@ echo %PATH%
 
 set DISABLE_REBUILD=true
 set NODE_REQUIRED_VERSION=12.13.1
+set SNYK_API_TOKEN=885953dc-9469-443c-984d-524352d54116
 
 set PATH=%PATH%;C:\Program Files\nodejs\;C:\Program Files\Git\cmd
 echo %PATH%
@@ -25,23 +26,21 @@ if %ERRORLEVEL% NEQ 0 (
   EXIT /B 1
 )
 
-nvm install %NODE_REQUIRED_VERSION%
-nvm use %NODE_REQUIRED_VERSION%
+call nvm install %NODE_REQUIRED_VERSION%
+call nvm use %NODE_REQUIRED_VERSION%
 
-WHERE node
+WHERE gulp
 if %ERRORLEVEL% NEQ 0 (
-  echo "NODE does not exist. Please set it up before running this script."
-  EXIT /B 1
+  echo "GULP does not exist. Installing it."
+  call npm i gulp -g
 )
 
-WHERE npm
+WHERE snyk
 if %ERRORLEVEL% NEQ 0 (
-  echo "NPM does not exist. Please set it up before running this script."
-  EXIT /B 1
+  echo "Snyk does not exist! Installing and setting it up"
+  call npm i snyk -g
+  call snyk config set api=%SNYK_API_TOKEN%
 )
-
-echo "Node version is: "
-call node --version
 
 :: Below command replaces buildVersion with the appropriate build number from jenkins
 sed -i -e "s/\"buildNumber\"[[:space:]]*\:[[:space:]]*\".*\"/\"buildNumber\":\"%PARENT_BUILD_VERSION%\"/g" package.json
@@ -50,8 +49,12 @@ sed -i -e "s/\"buildNumber\"[[:space:]]*\:[[:space:]]*\".*\"/\"buildNumber\":\"%
 echo "Copying search libraries"
 echo D | xcopy /y "C:\jenkins\workspace\tronlibraries\library" "library"
 
-echo "Running npm install..."
+echo "Installing dependencies..."
 call npm install
+
+# Run Snyk Security Tests
+echo "Running snyk security tests"
+call snyk test --file=package.json
 
 :: Set expiry if required
 IF "%EXPIRY_PERIOD%"=="" (
--- scripts/build-win64.bat
+++ scripts/build-win64.bat
@@ -6,6 +6,7 @@ echo %PATH%
 
 set DISABLE_REBUILD=true
 set NODE_REQUIRED_VERSION=12.13.1
+set SNYK_API_TOKEN=885953dc-9469-443c-984d-524352d54116
 
 set PATH=%PATH%;C:\Program Files\nodejs\;C:\Program Files\Git\cmd
 echo %PATH%
@@ -13,35 +14,21 @@ echo %PATH%
 set PATH=%PATH%;C:\Program Files (x86)\GnuWin32\bin
 echo %PATH%
 
-WHERE git
+call nvm install %NODE_REQUIRED_VERSION%
+call nvm use %NODE_REQUIRED_VERSION%
+
+WHERE gulp
 if %ERRORLEVEL% NEQ 0 (
-  echo "GIT does not exist. Please set it up before running this script."
-  EXIT /B 1
+  echo "GULP does not exist. Installing it."
+  call npm i gulp -g
 )
 
-WHERE nvm
+WHERE snyk
 if %ERRORLEVEL% NEQ 0 (
-  echo "NVM does not exist. Please set it up before running this script."
-  EXIT /B 1
+  echo "Snyk does not exist! Installing and setting it up"
+  call npm i snyk -g
+  call snyk config set api=%SNYK_API_TOKEN%
 )
-
-nvm install %NODE_REQUIRED_VERSION%
-nvm use %NODE_REQUIRED_VERSION%
-
-WHERE node
-if %ERRORLEVEL% NEQ 0 (
-  echo "NODE does not exist. Please set it up before running this script."
-  EXIT /B 1
-)
-
-WHERE npm
-if %ERRORLEVEL% NEQ 0 (
-  echo "NPM does not exist. Please set it up before running this script."
-  EXIT /B 1
-)
-
-echo "Node version is: "
-call node --version
 
 :: Below command replaces buildVersion with the appropriate build number from jenkins
 sed -i -e "s/\"buildNumber\"[[:space:]]*\:[[:space:]]*\".*\"/\"buildNumber\":\"%PARENT_BUILD_VERSION%\"/g" package.json
@@ -50,8 +37,12 @@ sed -i -e "s/\"buildNumber\"[[:space:]]*\:[[:space:]]*\".*\"/\"buildNumber\":\"%
 echo "Copying search libraries"
 echo D | xcopy /y "C:\jenkins\workspace\tronlibraries\library" "library"
 
-echo "Running npm install..."
+echo "Installing dependencies..."
 call npm install
+
+# Run Snyk Security Tests
+echo "Running snyk security tests"
+call snyk test --file=package.json
 
 :: Set expiry if required
 IF "%EXPIRY_PERIOD%"=="" (
