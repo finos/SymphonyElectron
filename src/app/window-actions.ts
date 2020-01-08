d871efb1958f98fc9b5ccfe09c2c09b532f678c2
Johan Kwarnmark <johan.kwarnmark@symphony.com>
2020-01-08 10:02:52 +0100

    sda-1657 remove save of position and size as before

--- src/app/window-actions.ts
+++ src/app/window-actions.ts
@@ -49,13 +49,11 @@ const saveWindowSettings = async (): Promise<void> => {
 const windowMaximized = async (): Promise<void> => {
     const browserWindow = BrowserWindow.getFocusedWindow() as ICustomBrowserWindow;
     if (browserWindow && windowExists(browserWindow)) {
-        const [ x, y ] = browserWindow.getPosition();
-        const [ width, height ] = browserWindow.getSize();
         const isMaximized = browserWindow.isMaximized();
         const isFullScreen = browserWindow.isFullScreen();
         if (browserWindow.winName === apiName.mainWindowName) {
             const { mainWinPos } = config.getUserConfigFields([ 'mainWinPos' ]);
-            await config.updateUserConfig({ mainWinPos: { ...mainWinPos, ...{ height, width, x, y, isMaximized, isFullScreen } } });
+            await config.updateUserConfig({ mainWinPos: { ...mainWinPos, ...{ isMaximized, isFullScreen } } });
         }
     }
 };
