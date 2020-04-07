e1a0acdb4e2d42e48e422df39ccd089c79af9441
Vishwas Shashidhar <VishwasShashidhar@users.noreply.github.com>
2020-04-07 16:05:52 +0530

    fix: SDA-1948: write snippet image to user data directory temp folder (#967)
    * SDA-1948: write snippet image to user data directory temp folder
    
    Signed-off-by: Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
    
    * SDA-1948: set tmp dir for tests
    
    Signed-off-by: Vishwas Shashidhar <vishwas.shashidhar@symphony.com>

--- src/app/screen-snippet-handler.ts
+++ src/app/screen-snippet-handler.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 import { ChildProcess, ExecException, execFile } from 'child_process';
 import * as util from 'util';
 import { IScreenSnippet } from '../common/api-interface';
-import { isDevEnv, isLinux, isMac, isWindowsOS } from '../common/env';
+import { isDevEnv, isElectronQA, isLinux, isMac, isWindowsOS } from '../common/env';
 import { i18n } from '../common/i18n';
 import { logger } from '../common/logger';
 import { updateAlwaysOnTop } from './window-actions';
@@ -25,7 +25,14 @@ class ScreenSnippet {
     private shouldUpdateAlwaysOnTop: boolean = false;
 
     constructor() {
-        this.tempDir = os.tmpdir();
+        if (isElectronQA) {
+            this.tempDir = os.tmpdir();
+        } else {
+            this.tempDir = path.join(app.getPath('userData'), 'temp');
+            if (!fs.existsSync(this.tempDir)) {
+                fs.mkdirSync(this.tempDir);
+            }
+        }
         this.captureUtil = isMac ? '/usr/sbin/screencapture' : isDevEnv
             ? path.join(__dirname,
                 '../../../node_modules/screen-snippet/ScreenSnippet.exe')
