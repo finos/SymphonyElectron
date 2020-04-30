0568aa79d3d6bbccf501b215c9e27517dc06f8a8
Mattias Gustavsson <mattias.gustavsson@symphony.com>
2020-04-30 13:13:39 +0200

    Invoke screen snippet tool with --no-annotate when running mana

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -156,7 +156,6 @@ export class WindowHandler {
         } catch (e) {
             throw new Error('failed to init crash report');
         }
-
     }
 
     /**
@@ -409,6 +408,7 @@ export class WindowHandler {
 
         // Handle pop-outs window
         handleChildWindow(this.mainWindow.webContents);
+
         return this.mainWindow;
     }
 
