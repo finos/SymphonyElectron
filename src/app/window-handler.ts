8ac95a95c14cbcf449f2cbb0b56321cac9b8827e
Vishwas Shashidhar <VishwasShashidhar@users.noreply.github.com>
2020-05-08 20:23:11 +0530

    SDA-2016: fix title bar issue (#989)
    Signed-off-by: Vishwas Shashidhar <vishwas.shashidhar@symphony.com>

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -264,14 +264,6 @@ export class WindowHandler {
         if (this.shouldShowWelcomeScreen) {
             this.handleWelcomeScreen();
         }
-        // loads the main window with url from config/cmd line
-        await this.mainWindow.loadURL(this.url);
-        // check for build expiry in case of test builds
-        await this.checkExpiry(this.mainWindow);
-        // update version info from server
-        this.updateVersionInfo();
-        // need this for postMessage origin
-        this.mainWindow.origin = this.url;
 
         // Event needed to hide native menu bar on Windows 10 as we use custom menu bar
         this.mainWindow.webContents.once('did-start-loading', () => {
@@ -426,6 +418,16 @@ export class WindowHandler {
 
         // Handle pop-outs window
         handleChildWindow(this.mainWindow.webContents);
+
+        // loads the main window with url from config/cmd line
+        await this.mainWindow.loadURL(this.url);
+        // check for build expiry in case of test builds
+        await this.checkExpiry(this.mainWindow);
+        // update version info from server
+        this.updateVersionInfo();
+        // need this for postMessage origin
+        this.mainWindow.origin = this.url;
+
         return this.mainWindow;
     }
 
