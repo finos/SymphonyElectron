a73e8810b0a7a23f6abbd2d39ab2ad67b988a781
Johan Kwarnmark <55975938+johankwarnmarksymphony@users.noreply.github.com>
2020-05-13 07:47:40 +0200

    fix: SDA-1738 RTC diagnotic start/close screen share quickly (#993)
    * sda-1738 RTC diagnotic start/close screen share quickly add timeout for 300ms
    
    * sda-1738 Move timeout value to a const
    
    Co-authored-by: Vishwas Shashidhar <VishwasShashidhar@users.noreply.github.com>

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -512,7 +512,8 @@ export class WindowHandler {
                     }
                 }
                 if (isWindowsOS || isMac) {
-                    this.execCmd(this.screenShareIndicatorFrameUtil, []);
+                    const timeoutValue = 300;
+                    setTimeout(() => this.execCmd(this.screenShareIndicatorFrameUtil, []), timeoutValue);
                 } else {
                     if (this.screenSharingFrameWindow && windowExists(this.screenSharingFrameWindow)) {
                         this.screenSharingFrameWindow.close();
@@ -743,10 +744,8 @@ export class WindowHandler {
                         } else {
                             const dispId = source.id.split(':')[1];
                             const keyId = 'id';
-
                             logger.info('window-utils: dispId: ' + dispId);
                             logger.info('window-utils: displays [' + dispId + '] [id]: ' + displays [dispId] [ keyId ]);
-
                             this.execCmd(this.screenShareIndicatorFrameUtil, [ displays [dispId] [ keyId ].toString() ]);
                         }
                     }
