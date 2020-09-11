bd96883b6b4594ab4a9a4c23377dba36f8364204
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2020-09-11 13:25:40 +0530

    SDA-2418: remove capturing extra system information

--- src/app/stats.ts
+++ src/app/stats.ts
@@ -1,6 +1,5 @@
 import { app } from 'electron';
 import * as os from 'os';
-import * as si from 'systeminformation';
 import { logger } from '../common/logger';
 
 export class AppStats {
@@ -17,7 +16,6 @@ export class AppStats {
         this.logAppMetrics();
         this.logConfigurationData();
         this.logAppEvents();
-        this.logAllStats();
     }
 
     /**
@@ -96,17 +94,6 @@ export class AppStats {
         logger.info(`stats: Resources Path? ${process.resourcesPath}`);
         logger.info(`stats: Chrome Version? ${process.versions.chrome}`);
         logger.info(`stats: Electron Version? ${process.versions.electron}`);
-    }
-
-    /**
-     * Log all system statistics
-     */
-    private async logAllStats() {
-        try {
-            logger.info(`All Data: ${JSON.stringify(await si.getAllData())}`);
-        } catch (e) {
-            logger.error(`Error gathering all data: ${e}`);
-        }
     }
 }
 
