b74aeaed3f360e61170a36a1cd844bff362fc0ee
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2020-10-13 16:09:54 +0530

    SDA-2559: clean app cache on install
    - Clean app cache on a fresh install of SDA to avoid upgradation problems
    - Clean app cache and restart SDA when it crashes or is unresponsive

--- spec/appCacheHandler.spec.ts
+++ spec/appCacheHandler.spec.ts
@@ -1,12 +1,27 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { cleanUpAppCache, createAppCacheFile } from '../src/app/app-cache-handler';
+import * as rimraf from 'rimraf';
+import { cleanAppCacheOnInstall, cleanUpAppCache, createAppCacheFile } from '../src/app/app-cache-handler';
 import { app, session } from './__mocks__/electron';
 
 jest.mock('fs', () => ({
-        writeFileSync: jest.fn(),
-        existsSync: jest.fn(() => true),
-        unlinkSync: jest.fn(),
+    writeFileSync: jest.fn(),
+    existsSync: jest.fn(() => true),
+    unlinkSync: jest.fn(),
+    readdirSync: jest.fn(() => ['fake1', 'fake2', 'Symphony.config', 'cloudConfig.config']),
+    lstatSync: jest.fn(() => {
+        return {
+            isDirectory: jest.fn(() => true),
+        };
+    }),
+}));
+
+jest.mock('path', () => ({
+    join: jest.fn(),
+}));
+
+jest.mock('rimraf', () => ({
+    sync: jest.fn(),
 }));
 
 jest.mock('../src/common/logger', () => {
@@ -19,27 +34,76 @@ jest.mock('../src/common/logger', () => {
 });
 
 describe('app cache handler', () => {
-    const cachePathExpected = path.join(app.getPath('userData'), 'CacheCheck');
+    describe('check app cache file', () => {
+        const cachePathExpected = path.join(app.getPath('userData'), 'CacheCheck');
 
-    it('should call `cleanUpAppCache` correctly', () => {
-        const spyFn = 'unlinkSync';
-        const spy = jest.spyOn(fs, spyFn);
-        cleanUpAppCache();
-        expect(spy).toBeCalledWith(cachePathExpected);
+        it('should call `cleanUpAppCache` correctly', () => {
+            const spyFn = 'unlinkSync';
+            const spy = jest.spyOn(fs, spyFn);
+            cleanUpAppCache();
+            expect(spy).toBeCalledWith(cachePathExpected);
+        });
+
+        it('should call `clearCache` when `session.defaultSession` is not null', () => {
+            jest.spyOn(fs, 'existsSync').mockImplementation(() => false);
+            const spyFn = 'clearCache';
+            const spy = jest.spyOn(session.defaultSession, spyFn);
+            cleanUpAppCache();
+            expect(spy).toBeCalled();
+        });
+
+        it('should call `createAppCacheFile` correctly', () => {
+            const spyFn = 'writeFileSync';
+            const spy = jest.spyOn(fs, spyFn);
+            createAppCacheFile();
+            expect(spy).lastCalledWith(cachePathExpected, '');
+        });
     });
 
-    it('should call `clearCache` when `session.defaultSession` is not null', () => {
-        jest.spyOn(fs, 'existsSync').mockImplementation(() => false);
-        const spyFn = 'clearCache';
-        const spy = jest.spyOn(session.defaultSession, spyFn);
-        cleanUpAppCache();
-        expect(spy).toBeCalled();
+    describe('clean app cache on install', () => {
+        it('should clean app cache and cookies on install', () => {
+
+            const pathSpy = jest.spyOn(path, 'join');
+
+            const fsReadDirSpy = jest.spyOn(fs, 'readdirSync');
+            const fsStatSpy = jest.spyOn(fs, 'lstatSync');
+            const fsUnlinkSpy = jest.spyOn(fs, 'unlinkSync');
+
+            const rimrafSpy = jest.spyOn(rimraf, 'sync');
+
+            cleanAppCacheOnInstall();
+
+            expect(pathSpy).toBeCalledTimes(10);
+
+            expect(fsReadDirSpy).toBeCalledTimes(1);
+            expect(fsStatSpy).toBeCalledTimes(2);
+            expect(fsUnlinkSpy).toBeCalledTimes(1);
+
+            expect(rimrafSpy).toBeCalledTimes(2);
+        });
     });
 
-    it('should call `createAppCacheFile` correctly', () => {
-        const spyFn = 'writeFileSync';
-        const spy = jest.spyOn(fs, spyFn);
-        createAppCacheFile();
-        expect(spy).lastCalledWith(cachePathExpected, '');
+    describe('clean app cache on crash', () => {
+        it('should clean app cache and cookies on crash', () => {
+
+            const pathSpy = jest.spyOn(path, 'join');
+
+            const fsReadDirSpy = jest.spyOn(fs, 'readdirSync');
+            const fsStatSpy = jest.spyOn(fs, 'lstatSync');
+            const fsUnlinkSpy = jest.spyOn(fs, 'unlinkSync');
+
+            const rimrafSpy = jest.spyOn(rimraf, 'sync');
+
+            cleanAppCacheOnInstall();
+
+            expect(pathSpy).toBeCalledTimes(10);
+
+            expect(fsReadDirSpy).toBeCalledTimes(1);
+            expect(fsStatSpy).toBeCalledTimes(2);
+            expect(fsUnlinkSpy).toBeCalledTimes(1);
+
+            expect(rimrafSpy).toBeCalledTimes(2);
+        });
     });
+
 });
--- src/app/app-cache-handler.ts
+++ src/app/app-cache-handler.ts
@@ -1,11 +1,36 @@
-import { app, session } from 'electron';
+import { app, BrowserWindow, session } from 'electron';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as rimraf from 'rimraf';
 
 import { logger } from '../common/logger';
 
 // Cache check file path
-const cacheCheckFilePath: string = path.join(app.getPath('userData'), 'CacheCheck');
+const userDataPath: string = app.getPath('userData');
+const cacheCheckFilePath: string = path.join(userDataPath, 'CacheCheck');
+
+/**
+ * Cleans old cache
+ */
+const cleanOldCache = (): void => {
+    const configFilename = 'Symphony.config';
+    const cloudConfigFilename = 'cloudConfig.config';
+
+    const files = fs.readdirSync(userDataPath);
+
+    files.forEach((file) => {
+        const filePath = path.join(userDataPath, file);
+        if (file === configFilename || file === cloudConfigFilename) {
+            return;
+        }
+
+        if (fs.lstatSync(filePath).isDirectory()) {
+            rimraf.sync(filePath);
+            return;
+        }
+        fs.unlinkSync(filePath);
+    });
+};
 
 /**
  * Deletes app cache file if exists or clears
@@ -29,4 +54,30 @@ export const cleanUpAppCache = async (): Promise<void> => {
 export const createAppCacheFile = (): void => {
     logger.info(`app-cache-handler: this is a clean exit, creating app cache file`);
     fs.writeFileSync(cacheCheckFilePath, '');
+};
+
+/**
+ * Cleans the app cache on new install
+ */
+export const cleanAppCacheOnInstall = (): void => {
+    logger.info(`app-cache-handler: cleaning app cache and cookies on new install`);
+    cleanOldCache();
+};
+
+/**
+ * Cleans app cache and restarts the app on crash or unresponsive events
+ * @param window Browser window to listen to for crash events
+ */
+export const cleanAppCacheOnCrash = (window: BrowserWindow): void => {
+    logger.info(`app-cache-handler: listening to crash events & cleaning app cache`);
+    const events = ['unresponsive', 'crashed', 'plugin-crashed'];
+
+    events.forEach((windowEvent: any) => {
+        window.webContents.on(windowEvent, () => {
+            logger.info(`app-cache-handler: Window Event '${windowEvent}' occurred. Clearing cache & restarting app`);
+            cleanOldCache();
+            app.relaunch();
+            app.exit();
+        });
+    });
 };
