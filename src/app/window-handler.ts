594ae8b5f58b2e80667d0b2b4578404a16ee8492
Salah Benmoussati <salah.benmoussati@symphony.com>
2022-01-19 09:14:40 +0100

    SDA-3534 Snipping tool width shouldn't be greater than screen width and height

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -1172,9 +1172,7 @@ export class WindowHandler {
     // Snipping tool height shouldn't be greater than min of screen width and height (screen orientation can be portrait or landscape)
     const minSize = Math.min(workAreaSize.width, workAreaSize.height);
     const maxToolHeight = Math.floor(calculatePercentage(minSize, 90));
-    const maxToolWidth = Math.floor(
-      calculatePercentage(workAreaSize.width, 90),
-    );
+    const maxToolWidth = Math.floor(calculatePercentage(minSize, 90));
     const availableAnnotateAreaHeight = maxToolHeight - BUTTON_BARS_HEIGHT;
     const availableAnnotateAreaWidth = maxToolWidth;
     const scaleFactor = display.scaleFactor;
