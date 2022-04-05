80f96bba36e0b8ca7efb0db68a0851b5849b6f21
Salah Benmoussati <51402489+sbenmoussati@users.noreply.github.com>
2022-04-05 13:17:49 +0200

    Bugfix/rtc 17.0.x (#1372)
    * Initial implementation (#1362)
    
    * SDA-3616 - Upgrade ffi-napi and ref-napi (#1364)
    
    * Remove duplicated ffi-napi and ref-napi packages
    
    * Update matching types packages
    
    Co-authored-by: Robin Westberg <robin.westberg@symphony.com>

--- src/app/main-api-handler.ts
+++ src/app/main-api-handler.ts
@@ -21,6 +21,7 @@ import appStateHandler from './app-state-handler';
 import { getCitrixMediaRedirectionStatus } from './citrix-handler';
 import { CloudConfigDataTypes, config, ICloudConfig } from './config-handler';
 import { downloadHandler } from './download-handler';
+import { getContentWindowHandle } from './hwnd-handler';
 import { mainEvents } from './main-event-handler';
 import { memoryMonitor } from './memory-monitor';
 import notificationHelper from './notifications/notification-helper';
@@ -459,7 +460,8 @@ ipcMain.handle(
           event.sender,
         ) as ICustomBrowserWindow;
         if (browserWin && windowExists(browserWin)) {
-          return browserWin.getNativeWindowHandle();
+          const windowHandle = browserWin.getNativeWindowHandle();
+          return getContentWindowHandle(windowHandle);
         }
         break;
       case apiCmds.getCitrixMediaRedirectionStatus:
