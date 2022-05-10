e21ceb4fdc95deff59f9afc0b618924460106a0d
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-05-10 08:17:21 +0530

    Bump napi

--- scripts/build-win64.bat
+++ scripts/build-win64.bat
@@ -5,7 +5,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tool
 echo %PATH%
 
 set DISABLE_REBUILD=true
-set NODE_REQUIRED_VERSION=16.13.2
+set NODE_REQUIRED_VERSION=16.15.0
 set SNYK_ORG=sda
 set SNYK_PROJECT_NAME="Symphony Desktop Application"
 
