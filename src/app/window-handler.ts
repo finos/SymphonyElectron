de2cd22d14087a067b10bf0798f4d07a1f4c03b3
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-05-20 06:44:25 +0530

    SDA-3656 - Close placeholder window only on Windows

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -1501,12 +1501,14 @@ export class WindowHandler {
       ipcMain.removeListener('screen-source-select', screenSourceSelect);
       this.removeWindow(opts.winKey);
       this.screenPickerWindow = null;
-      if (
-        this.screenPickerPlaceholderWindow &&
-        windowExists(this.screenPickerPlaceholderWindow)
-      ) {
-        this.screenPickerPlaceholderWindow.close();
-        this.screenPickerPlaceholderWindow = null;
+      if (isWindowsOS) {
+        if (
+          this.screenPickerPlaceholderWindow &&
+          windowExists(this.screenPickerPlaceholderWindow)
+        ) {
+          this.screenPickerPlaceholderWindow.close();
+          this.screenPickerPlaceholderWindow = null;
+        }
       }
     });
   }
