86603c9c7039fae5249c3a656cbaf326d992057e
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-06-20 14:21:39 +0530

    SDA-3588 - Validate url

--- src/app/auto-update-handler.ts
+++ src/app/auto-update-handler.ts
@@ -10,6 +10,9 @@ class AutoUpdate {
 
   constructor() {
     const { autoUpdateUrl } = config.getGlobalConfigFields(['autoUpdateUrl']);
+    if (autoUpdateUrl === '') {
+      return;
+    }
     if (isMac) {
       this.autoUpdater = new MacUpdater(autoUpdateUrl);
     } else if (isWindowsOS) {
