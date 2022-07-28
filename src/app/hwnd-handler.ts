44f6bb54611725ea30d37b4dc2beacec51c7001a
Robin Westberg <robin.westberg@symphony.com>
2022-07-28 11:37:00 +0200

    SDA-3707 - Fix getting window handle for maximized windows (#1420) (#1427)

--- src/app/hwnd-handler.ts
+++ src/app/hwnd-handler.ts
@@ -38,10 +38,8 @@ export const getContentWindowHandle = (nativeWindowHandle: Buffer): Buffer => {
   };
 
   const parentHwnd = nativeWindowHandle.readBigUInt64LE();
-  const parentRect = getWindowRect(parentHwnd);
-  if (!parentRect) {
-    return nativeWindowHandle;
-  }
+  const children = new Map();
+  let titleChild;
 
   let child = user32.FindWindowExA(
     parentHwnd.toString(),
@@ -51,13 +49,18 @@ export const getContentWindowHandle = (nativeWindowHandle: Buffer): Buffer => {
   );
   while (child !== 0) {
     const rect = getWindowRect(child);
+    if (rect) {
+      children.set(child, rect);
 
-    // The candidate child window is located at the same x position as the parent window, but
-    // has a higher y position (due to the window title frame at the top).
-    if (rect && parentRect.left === rect.left && parentRect.top < rect.top) {
-      const ret = Buffer.alloc(8);
-      ret.writeBigUInt64LE(BigInt(child));
-      return ret;
+      // The titlebar child is the one that is the least tall
+      if (!titleChild) {
+        titleChild = child;
+      } else {
+        const curTitleRect = children.get(titleChild)!;
+        if (rect.bottom - rect.top < curTitleRect.bottom - curTitleRect.top) {
+          titleChild = child;
+        }
+      }
     }
     child = user32.FindWindowExA(
       parentHwnd.toString(),
@@ -66,5 +69,15 @@ export const getContentWindowHandle = (nativeWindowHandle: Buffer): Buffer => {
       null,
     );
   }
+
+  // Return the first child window that isn't the title bar
+  for (const hwnd of children.keys()) {
+    if (hwnd !== titleChild) {
+      const ret = Buffer.alloc(8);
+      ret.writeBigUInt64LE(BigInt(hwnd));
+      return ret;
+    }
+  }
+
   return nativeWindowHandle;
 };
