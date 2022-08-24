d5e8471964fe990264642b20aa96335d6895f030
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-08-24 15:11:06 +0530

    SDA-3815 - Disable maximize button and enable minimize button

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -728,7 +728,7 @@ export class WindowHandler {
         frame: !this.isCustomTitleBar,
         alwaysOnTop: isMac,
         resizable: false,
-        minimizable: false,
+        minimizable: true,
         fullscreenable: false,
       },
       {
