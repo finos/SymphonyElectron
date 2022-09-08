153ee6aa9a2d46539e31ee733f8225a2c44181e6
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-09-08 15:23:53 +0530

    SDA-3844 - Add menu items for corp switch

--- src/app/app-menu.ts
+++ src/app/app-menu.ts
@@ -24,7 +24,11 @@ import {
   unregisterConsoleMessages,
   updateAlwaysOnTop,
 } from './window-actions';
-import { ICustomBrowserWindow, windowHandler } from './window-handler';
+import {
+  ClientSwitchType,
+  ICustomBrowserWindow,
+  windowHandler,
+} from './window-handler';
 import {
   reloadWindow,
   resetZoomLevel,
@@ -92,6 +96,7 @@ let {
   isAutoUpdateEnabled,
 } = config.getConfigFields(menuItemConfigFields) as IConfig;
 let initialAnalyticsSent = false;
+const CORP_URL = 'https://corporate.symphony.com';
 
 const menuItemsArray = Object.keys(menuSections)
   .map((key) => menuSections[key])
@@ -582,6 +587,10 @@ export class AppMenu {
 
     const { devToolsEnabled: isDevToolsEnabledCC } = this
       .cloudConfig as IConfig;
+    const isCorp =
+      (windowHandler.url &&
+        windowHandler.url.startsWith('https://corporate.symphony.com')) ||
+      false;
 
     return {
       label: i18n.t('Help')(),
@@ -681,6 +690,30 @@ export class AppMenu {
               : '';
             windowHandler.createAboutAppWindow(windowName);
           },
+        },
+        {
+          click: (_item) =>
+            windowHandler.switchClient(ClientSwitchType.CLIENT_1_5),
+          visible: isCorp,
+          type: 'checkbox',
+          checked: windowHandler.url?.startsWith(CORP_URL + '/client/'),
+          label: i18n.t('Switch to client 1.5')(),
+        },
+        {
+          click: (_item) =>
+            windowHandler.switchClient(ClientSwitchType.CLIENT_2_0),
+          visible: isCorp,
+          type: 'checkbox',
+          checked: windowHandler.url?.startsWith(CORP_URL + '/client-bff'),
+          label: i18n.t('Switch to client 2.0')(),
+        },
+        {
+          click: (_item) =>
+            windowHandler.switchClient(ClientSwitchType.CLIENT_2_0_DAILY),
+          visible: isCorp,
+          type: 'checkbox',
+          checked: windowHandler.url?.startsWith(CORP_URL + '/bff-daily/daily'),
+          label: i18n.t('Switch to client 2.0 daily')(),
         },
       ],
     };
--- src/app/local-menu-shortcuts.ts
+++ src/app/local-menu-shortcuts.ts
@@ -1,6 +1,6 @@
 import { Menu, MenuItem, MenuItemConstructorOptions } from 'electron';
 import { isMac } from '../common/env';
-import { ClientSwitchType, windowHandler } from './window-handler';
+import { windowHandler } from './window-handler';
 import { resetZoomLevel, zoomIn, zoomOut } from './window-utils';
 
 export default class LocalMenuShortcuts {
@@ -38,39 +38,6 @@ export default class LocalMenuShortcuts {
         }),
       ),
     );
-
-    if (
-      windowHandler.url &&
-      windowHandler.url.startsWith('https://corporate.symphony.com')
-    ) {
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+1' : 'Ctrl+Shift+1',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_1_5),
-          }),
-        ),
-      );
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+2' : 'Ctrl+Shift+2',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_2_0),
-          }),
-        ),
-      );
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+3' : 'Ctrl+Shift+3',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_2_0_DAILY),
-          }),
-        ),
-      );
-    }
 
     this.menu.append(
       new MenuItem(
