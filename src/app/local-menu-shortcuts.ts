aba73978b243fb79c3021279c9fe5859c7d055cb
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-09-09 10:27:59 +0530

    SDA-3844 (Use local shortcuts to fix issues on Windows) (#1488)
    * SDA-3844 - Use local shortcuts to fix issues on Windows
    
    * SDA-3844 - Remove duplicate menu item
    
    * SDA-3844 - Add menu items for corp switch
    
    * SDA-3844 - Workaround for export logs local shortcut
    
    * SDA-3844 - Fix translations

---
+++ src/app/local-menu-shortcuts.ts
@@ -0,0 +1,98 @@
+import { Menu, MenuItem, MenuItemConstructorOptions } from 'electron';
+import { isMac } from '../common/env';
+import { windowHandler } from './window-handler';
+import { resetZoomLevel, zoomIn, zoomOut } from './window-utils';
+
+export default class LocalMenuShortcuts {
+  private menu: Menu;
+
+  constructor() {
+    this.menu = new Menu();
+  }
+
+  public buildShortcutMenu = () => {
+    // Devtools shortcut
+    this.menu.append(
+      new MenuItem(
+        this.getMenuItemOptions({
+          accelerator: isMac ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click: () => windowHandler.onRegisterDevtools(),
+        }),
+      ),
+    );
+    // Reload shortcut
+    this.menu.append(
+      new MenuItem(
+        this.getMenuItemOptions({
+          accelerator: 'CmdOrCtrl+R',
+          click: () => windowHandler.onReload(),
+        }),
+      ),
+    );
+
+    this.menu.append(
+      new MenuItem(
+        this.getMenuItemOptions({
+          accelerator: 'CmdOrCtrl+=',
+          click: () => zoomIn(),
+        }),
+      ),
+    );
+    this.menu.append(
+      new MenuItem(
+        this.getMenuItemOptions({
+          accelerator: 'CmdOrCtrl+-',
+          click: () => zoomOut(),
+        }),
+      ),
+    );
+
+    if (isMac) {
+      this.menu.append(
+        new MenuItem(
+          this.getMenuItemOptions({
+            accelerator: 'CmdOrCtrl+Plus',
+            click: () => zoomIn(),
+          }),
+        ),
+      );
+    } else {
+      this.menu.append(
+        new MenuItem(
+          this.getMenuItemOptions({
+            accelerator: 'Ctrl+numadd',
+            click: () => zoomIn(),
+          }),
+        ),
+      );
+      this.menu.append(
+        new MenuItem(
+          this.getMenuItemOptions({
+            accelerator: 'Ctrl+numsub',
+            click: () => zoomOut(),
+          }),
+        ),
+      );
+      this.menu.append(
+        new MenuItem(
+          this.getMenuItemOptions({
+            accelerator: 'Ctrl+num0',
+            click: () => resetZoomLevel(),
+          }),
+        ),
+      );
+    }
+
+    Menu.setApplicationMenu(this.menu);
+  };
+
+  private getMenuItemOptions = (opts): MenuItemConstructorOptions => {
+    return {
+      ...{
+        visible: false,
+        acceleratorWorksWhenHidden: true,
+      },
+      ...opts,
+    };
+  };
+}
