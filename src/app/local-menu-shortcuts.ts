58e10cf2bd69351ac5170a89a215a1d38ab8e6f6
Kiran Niranjan <kiran.niranjan@symphony.com>
2022-09-09 10:27:53 +0530

    SDA-3844 (Add menu items for corp switch) (#1491)
    * SDA-3844 - Add menu items for corp switch
    
    * SDA-3844 - Workaround for export logs local shortcut
    
    * SDA-3844 - Fix translations

--- src/app/local-menu-shortcuts.ts
+++ src/app/local-menu-shortcuts.ts
@@ -1,6 +1,6 @@
 import { Menu, MenuItem, MenuItemConstructorOptions } from 'electron';
 import { isMac } from '../common/env';
-import { ClientSwitchType, windowHandler } from './window-handler';
+import { windowHandler } from './window-handler';
 import { resetZoomLevel, zoomIn, zoomOut } from './window-utils';
 
 export default class LocalMenuShortcuts {
@@ -29,48 +29,6 @@ export default class LocalMenuShortcuts {
         }),
       ),
     );
-    // Export logs
-    this.menu.append(
-      new MenuItem(
-        this.getMenuItemOptions({
-          accelerator: 'Ctrl+Shift+D',
-          click: () => windowHandler.onExportLogs(),
-        }),
-      ),
-    );
-
-    if (
-      windowHandler.url &&
-      windowHandler.url.startsWith('https://corporate.symphony.com')
-    ) {
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+1' : 'Ctrl+Shift+1',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_1_5),
-          }),
-        ),
-      );
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+2' : 'Ctrl+Shift+2',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_2_0),
-          }),
-        ),
-      );
-      this.menu.append(
-        new MenuItem(
-          this.getMenuItemOptions({
-            accelerator: isMac ? 'Cmd+Alt+3' : 'Ctrl+Shift+3',
-            click: () =>
-              windowHandler.switchClient(ClientSwitchType.CLIENT_2_0_DAILY),
-          }),
-        ),
-      );
-    }
 
     this.menu.append(
       new MenuItem(
