eec7813de6602841c95c3d0494cd059e5798379c
sbenmoussati <51402489+sbenmoussati@users.noreply.github.com>
2022-11-25 08:51:19 +0100

    Updating installers CDN URL

--- scripts/macos_update_yml.js
+++ scripts/macos_update_yml.js
@@ -4,12 +4,12 @@ const crypto = require('crypto');
 
 const yaml = require('js-yaml');
 
-const INSTALLERS = 'installers/';
+const INSTALLERS_URL = 'https://static.symphony.com/sda/';
 
 function updateYamlFile(yamlFilePath) {
   let doc = yaml.load(fs.readFileSync(yamlFilePath, 'utf-8'));
-  doc.files[0].url = INSTALLERS + doc.files[0].url;
-  doc.path = INSTALLERS + doc.path;
+  doc.files[0].url = INSTALLERS_URL + doc.files[0].url;
+  doc.path = INSTALLERS_URL + doc.path;
   fs.writeFileSync(yamlFilePath, yaml.dump(doc, { lineWidth: -1 }));
 }
 
