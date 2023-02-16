786dd243cd444d55abb8c68aa9a0f1ba13d460fc
Swapna Sundar Biswal <63641254+swasunb@users.noreply.github.com>
2023-02-16 15:40:20 +0530

    RTC:13399 Forward proxy returned by electron to Cloud9 shell on launch (#1706)
    * forward proxyServer received from session.resolveProxy to Cloud9 app on launch
    
    * return proxy as we get it only replacing PROXY

--- spec/c9ShellHandler.spec.ts
+++ spec/c9ShellHandler.spec.ts
@@ -1,5 +1,11 @@
 describe('C9 shell handler', () => {
-  const webContentsMocked = { send: jest.fn() };
+  const webContentsMocked = {
+    send: jest.fn(),
+    session: {
+      resolveProxy: jest.fn(),
+    },
+    getURL: jest.fn(),
+  };
   const mockSpawnEvents = new Map<String, any>();
   const mockSpawn = jest.fn();
   const mockGetCommandLineArgs = jest.fn();
@@ -44,13 +50,16 @@ describe('C9 shell handler', () => {
         kill: mockKill,
       };
     });
+    webContentsMocked.session.resolveProxy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(''));
     mockIsWindows = true;
   });
 
   describe('launch', () => {
-    it('success', () => {
+    it('success', async () => {
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'starting' },
       });
@@ -61,9 +70,9 @@ describe('C9 shell handler', () => {
       });
     });
 
-    it('failure', () => {
+    it('failure', async () => {
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'starting' },
       });
@@ -74,19 +83,19 @@ describe('C9 shell handler', () => {
       });
     });
 
-    it('with attach', () => {
+    it('with attach', async () => {
       mockGetCommandLineArgs.mockReturnValue('--c9pipe=custompipe');
 
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'active', pipeName: 'symphony-c9-custompipe' },
       });
     });
 
-    it('cached status on relaunch', () => {
+    it('cached status on relaunch', async () => {
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'starting' },
       });
@@ -102,34 +111,70 @@ describe('C9 shell handler', () => {
       });
     });
 
-    it('args', () => {
+    it('args', async () => {
       mockGetGuid.mockReturnValue('just-another-guid');
 
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(mockSpawn).toBeCalledWith(
         expect.stringContaining('c9shell.exe'),
-        ['--symphonyHost', 'just-another-guid'],
+        ['--symphonyHost', 'just-another-guid', '--proxyServer', ''],
         { stdio: 'pipe' },
       );
     });
 
-    it('non-windows', () => {
-      mockIsWindows = false;
-
+    it('args, when resolveProxy returns DIRECT', async () => {
+      webContentsMocked.session.resolveProxy = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve('DIRECT'));
+      mockGetGuid.mockReturnValue('just-another-guid');
       const { loadC9Shell } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+
+      await loadC9Shell(webContentsMocked as any);
+
+      expect(mockSpawn).toBeCalledWith(
+        expect.stringContaining('c9shell.exe'),
+        ['--symphonyHost', 'just-another-guid', '--proxyServer', 'DIRECT'],
+        { stdio: 'pipe' },
+      );
+    });
+
+    it('args, when resolveProxy returns string starting with PROXY ', async () => {
+      webContentsMocked.session.resolveProxy = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve('PROXY 52.207.140.132:8443'));
+      mockGetGuid.mockReturnValue('just-another-guid');
+      const { loadC9Shell } = require('../src/app/c9-shell-handler');
+
+      await loadC9Shell(webContentsMocked as any);
+
+      expect(mockSpawn).toBeCalledWith(
+        expect.stringContaining('c9shell.exe'),
+        [
+          '--symphonyHost',
+          'just-another-guid',
+          '--proxyServer',
+          '52.207.140.132:8443',
+        ],
+        { stdio: 'pipe' },
+      );
+    });
+
+    it('non-windows', async () => {
+      mockIsWindows = false;
+      const { loadC9Shell } = require('../src/app/c9-shell-handler');
+      await loadC9Shell(webContentsMocked as any);
       expect(mockSpawn).not.toBeCalled();
     });
   });
 
   describe('terminate', () => {
-    it('success', () => {
+    it('success', async () => {
       const {
         loadC9Shell,
         terminateC9Shell,
       } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'starting' },
       });
@@ -144,12 +189,12 @@ describe('C9 shell handler', () => {
       expect(mockKill).toBeCalledTimes(0);
     });
 
-    it('no terminate if already exited', () => {
+    it('no terminate if already exited', async () => {
       const {
         loadC9Shell,
         terminateC9Shell,
       } = require('../src/app/c9-shell-handler');
-      loadC9Shell(webContentsMocked as any);
+      await loadC9Shell(webContentsMocked as any);
       expect(webContentsMocked.send).lastCalledWith('c9-status-event', {
         status: { status: 'starting' },
       });
