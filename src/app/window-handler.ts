2e488b6dafd0a3cd0d997245acf97678a24026fe
sbenmoussati <51402489+sbenmoussati@users.noreply.github.com>
2023-04-13 16:23:58 +0200

    SDA-4159 C2 version bugfix for daily

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -1145,7 +1145,7 @@ export class WindowHandler {
   /**
    * Creates a about app window
    */
-  public createAboutAppWindow(windowName: string): void {
+  public async createAboutAppWindow(windowName: string): Promise<void> {
     // This prevents creating multiple instances of the
     // about window
     if (didVerifyAndRestoreWindow(this.aboutAppWindow)) {
@@ -1179,6 +1179,8 @@ export class WindowHandler {
         this.aboutAppWindow = null;
       }
     };
+
+    await versionHandler.getClientVersion(true, this.url);
 
     if (
       this.mainWindow &&
