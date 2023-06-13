76001629b842dd6ac90e4c749ba7d11811564aec
sbenmoussati <salah.benmoussati@symphony.com>
2023-06-13 11:49:05 +0200

    SDA-4206 Set alwaysOnTop and skipTaskbar post hiding main window

--- src/app/stores/window-store.ts
+++ src/app/stores/window-store.ts
@@ -1,7 +1,11 @@
 import { BrowserWindow } from 'electron';
+import { presenceStatusStore } from '.';
+import { apiName } from '../../common/api-interface';
 import { isMac, isWindowsOS } from '../../common/env';
+import { logger } from '../../common/logger';
+import { presenceStatus } from '../presence-status-handler';
 import { ICustomBrowserWindow, windowHandler } from '../window-handler';
-import { getWindowByName } from '../window-utils';
+import { getWindowByName, showBadgeCount } from '../window-utils';
 
 export interface IWindowObject {
   windows: IWindowState[];
@@ -55,6 +59,7 @@ export class WindowStore {
 
   public restoreWindows = (hideOnCapture?: boolean) => {
     if (hideOnCapture) {
+      this.restoreNotificationProperties();
       const storedWindows = this.getWindowStore();
       let currentWindow = storedWindows.windows.find(
         (currentWindow) => currentWindow.focused,
@@ -102,9 +107,36 @@ export class WindowStore {
         focusedWindowToRestore,
       );
 
+      showBadgeCount(presenceStatusStore.getNotificationCount());
+      const mainWindow = windowHandler.getMainWindow();
+      if (mainWindow) {
+        const items = presenceStatus.createThumbarButtons();
+        presenceStatus.updateSystemTrayPresence();
+        mainWindow?.setThumbarButtons(items);
+        logger.info('window-store: restoring thumbnail toolbar buttons');
+      }
       // Store reset
       this.destroyWindowStore();
     }
+  };
+
+  /**
+   * Restores notification properties reset to default post hiding main window
+   * @param windowsNames
+   */
+  private restoreNotificationProperties = () => {
+    const windows = BrowserWindow.getAllWindows();
+
+    windows
+      .filter(
+        (window) =>
+          (window as ICustomBrowserWindow).winName ===
+          apiName.notificationWindowName,
+      )
+      .map((notificationWindow) => {
+        notificationWindow.setAlwaysOnTop(true);
+        notificationWindow.setSkipTaskbar(true);
+      });
   };
 
   private hideFullscreenWindow = (window: BrowserWindow) => {
