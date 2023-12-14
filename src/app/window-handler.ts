06368d8e7b5a35ec3184b3ed61a489aa0471b689
Kiran Niranjan <kiran.niranjan@symphony.com>
2023-12-14 13:41:38 +0530

    SDA-4417 - Move focus listener outside did-finish-load and use window property instead of config for AlwaysOnTop (#2031)

--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -638,20 +638,18 @@ export class WindowHandler {
           MAIN_WEB_CONTENTS_EVENTS,
           this.mainWebContents,
         );
+      }
+    });
 
-        // workaround for https://perzoinc.atlassian.net/browse/SDA-4251
-        this.mainWindow?.on('focus', () => {
-          const { alwaysOnTop } = config.getConfigFields(['alwaysOnTop']);
-          logger.info('window-handler: main window focused', alwaysOnTop);
-          if (
-            alwaysOnTop === CloudConfigDataTypes.ENABLED &&
-            this.mainWindow &&
-            windowExists(this.mainWindow)
-          ) {
-            this.mainWindow.setAlwaysOnTop(false);
-            this.mainWindow.setAlwaysOnTop(true);
-          }
-        });
+    // workaround for https://perzoinc.atlassian.net/browse/SDA-4251
+    this.mainWindow?.on('focus', () => {
+      if (
+        this.mainWindow &&
+        windowExists(this.mainWindow) &&
+        this.mainWindow.isAlwaysOnTop()
+      ) {
+        this.mainWindow.setAlwaysOnTop(false);
+        this.mainWindow.setAlwaysOnTop(true);
       }
     });
 
