c015fc2e915e4f7ed426f4b64046474d9062fa22
Kiran Niranjan <kiran.niranjan@symphony.com>
2024-03-25 13:36:00 +0530

    SDA-4507 - Read from default after copying global config

--- src/app/config-handler.ts
+++ src/app/config-handler.ts
@@ -824,6 +824,8 @@ class Config {
           this.globalConfig as IConfig,
           appGlobalConfigData,
         );
+        // After everything is set from previous SDA version
+        this.globalConfig = getAllUserDefaults();
         return;
       }
       if (!this.installVariant || this.installVariant === '') {
