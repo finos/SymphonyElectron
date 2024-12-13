85585d858bf68f67aaec11ca30ae7c8e3af5a1f5
Kiran Niranjan <kiran.niranjan@symphony.com>
2024-12-13 15:46:11 +0530

    SDA-4682 - increase child window minimum size

--- src/app/child-window-handler.ts
+++ src/app/child-window-handler.ts
@@ -40,8 +40,8 @@ import {
 const DEFAULT_POP_OUT_WIDTH = 300;
 const DEFAULT_POP_OUT_HEIGHT = 600;
 
-const MIN_WIDTH = 300;
-const MIN_HEIGHT = 300;
+const MIN_WIDTH = 400;
+const MIN_HEIGHT = 400;
 
 const CHILD_WINDOW_EVENTS = ['enter-full-screen', 'leave-full-screen'];
 
