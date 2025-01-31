2e4dceef7a29747fe392d87afb1a10aba805419b
Salah Benmoussati <51402489+sbenmoussati@users.noreply.github.com>
2025-01-31 11:49:09 +0100

    Revert "Use setDisplayMediaRequestHandler instead (#2266)" (#2275)
    This reverts commit 1b83b36f55efd4652fb583e6eff69080c7fcbfbf.

--- src/app/main.ts
+++ src/app/main.ts
@@ -8,7 +8,6 @@ import { cleanUpAppCache, createAppCacheFile } from './app-cache-handler';
 import { setChromeFlags, setSessionProperties } from './chrome-flags';
 import { config } from './config-handler';
 import './dialog-handler';
-import { setDisplayMediaRequestHandler } from './display-media-request-handler';
 import './main-api-handler';
 import { handlePerformanceSettings } from './perf-handler';
 import { protocolHandler } from './protocol-handler';
@@ -109,7 +108,6 @@ const startApplication = async () => {
   // Picks global config values and updates them in the user config
   await config.updateUserConfigOnStart();
   setSessionProperties();
-  setDisplayMediaRequestHandler();
   await autoUpdate.init();
   await windowHandler.createApplication();
   logger.info(`main: created application`);
