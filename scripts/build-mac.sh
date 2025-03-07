56b57a342a4e993dc439698a845ede456740e6e9
Kiran Niranjan <kiran.niranjan@symphony.com>
2025-03-07 11:31:07 +0530

    SDA-4832 - Bump SDA version 25.4.0 & electron version 35.0.0

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -4,7 +4,7 @@
 echo "Unlocking keychain"
 security -v unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
 
-NODE_REQUIRED_VERSION=v20.18.1
+NODE_REQUIRED_VERSION=v22.14.0
 SNYK_ORG=sda
 SNYK_PROJECT_NAME="Symphony Desktop Application"
 
