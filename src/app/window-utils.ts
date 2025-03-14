a48996b09cac567bc8fa0ceb30765ecc232cbe43
Kiran Niranjan <kiran.niranjan@symphony.com>
2025-03-14 14:16:36 +0530

    SDA-4847 - SDA-4848 - Optimize mini view and fix mini state (#2332)
    * SDA-4847 - SDA-4848 - Optimize mini view and fix mini state
    
    * SDA-4847 - SDA-4848 - Refactor typo

--- src/app/window-utils.ts
+++ src/app/window-utils.ts
@@ -55,6 +55,7 @@ import { notification } from '../renderer/notification';
 import { autoLaunchInstance } from './auto-launch-controller';
 import { autoUpdate, AutoUpdateTrigger } from './auto-update-handler';
 import { mainEvents } from './main-event-handler';
+import { miniViewHandler } from './mini-view-handler';
 import { openfinHandler } from './openfin-handler';
 import { presenceStatus } from './presence-status-handler';
 import { presenceStatusStore } from './stores';
@@ -1407,6 +1408,16 @@ export const loadWebContentsView = async (
       });
     }, 500);
     mainEvents.publish('enter-full-screen');
+    if (
+      windowHandler.getIsMiniViewFeatureEnabled() &&
+      windowHandler.getIsMiniViewEnabled()
+    ) {
+      miniViewHandler.notifyClient(false);
+      const appMenu = windowHandler.appMenu;
+      if (appMenu) {
+        appMenu.buildMenu();
+      }
+    }
   });
   mainWindow.on('leave-full-screen', () => {
     logger.info('EVENT leave-full-screen!!');
