380c71aa48984c5fa8fd1bba37a995e1d1e77b24
NguyenTranHoangSym <97150869+NguyenTranHoangSym@users.noreply.github.com>
2025-06-03 13:47:47 +0700

    SDA-4909_Main: Revert support including IV logs into bundle (#2388)
    * Revert "SDA-4909_25.5: Neutralize Path if they arent exist (#2385)"
    
    This reverts commit be0cea52cf443dac3fccc5846a91e628ccec7d42.
    
    * Revert "SDA-4909: Adding log handling exception for IV"
    
    This reverts commit 0ca11a433cd0b4a75e503f03407fb9d2311bedd3.
    
    * Revert "SDA-4415: Fix comments"
    
    This reverts commit 2fe830a3ed2a44ae277ef89b4fdfb8a7ef13ccd0.
    
    * Revert "SDA-4415: Adding UTs"
    
    This reverts commit e93942043587c3a3f41385aa1f68deb6630f66cb.
    
    * Revert "SDA-4415: Finalizing the approach on working with files"
    
    This reverts commit 0e7590439980dbcaafcd7a3c2c3d4a1a30be8dac.

--- src/app/reports-handler.ts
+++ src/app/reports-handler.ts
@@ -1,19 +1,12 @@
 import * as zip from 'adm-zip';
 import { app, BrowserWindow, dialog, shell, WebContents } from 'electron';
 import * as fs from 'fs';
-import { homedir as getHomedir } from 'os';
 import * as path from 'path';
 
 import { ILogs } from '../common/api-interface';
 import { isLinux, isMac, isWindowsOS } from '../common/env';
 import { i18n } from '../common/i18n';
 import { logger } from '../common/logger';
-import { fileHelper } from './helpers/file/files-helper';
-import {
-  LogCategory,
-  RemoveIVLogs,
-  RetrieveIVLogs,
-} from './interfaces/reports-handlers.interface';
 
 /**
  * Archives files in the source directory
@@ -72,10 +65,14 @@ let logWebContents: WebContents;
 const logTypes: string[] = [];
 const receivedLogs: ILogs[] = [];
 
-const writeLogs = async (retrievedLogs: ILogs[]) => {
+const validateFilename = (filename: string): string => {
+  return filename?.replace(/[^a-zA-Z0-9/_\.-]/g, '_');
+};
+
+const writeClientLogs = async (retrievedLogs: ILogs[]) => {
   for await (const logs of retrievedLogs) {
     for (const logFile of logs.logFiles) {
-      const sanitizedFilename = fileHelper.validateFilename(logFile.filename);
+      const sanitizedFilename = validateFilename(logFile.filename);
       if (!sanitizedFilename) {
         continue;
       }
@@ -126,12 +123,7 @@ export const packageLogs = async (retrievedLogs: ILogs[]): Promise<void> => {
   const timestamp = new Date().getTime();
   const destination = app.getPath('downloads') + destPath + timestamp + '.zip';
 
-  if (isWindowsOS) {
-    removeLastIVLogs(logsPath);
-    extractIVLogs(logsPath);
-  }
-
-  await writeLogs(retrievedLogs);
+  await writeClientLogs(retrievedLogs);
   generateArchiveForDirectory(
     logsPath,
     destination,
@@ -285,60 +277,4 @@ const writeDataToFile = async (filePath: string, data: string) => {
   }
 
   writeStream.end();
-};
-
-const removeLastIVLogs: RemoveIVLogs = (logsPath: string) => {
-  const ivLogsList = fileHelper.getLatestModifiedFiles(logsPath, {
-    includeOnly: [LogCategory.C9_TRADER, LogCategory.C9_ZUES],
-  });
-
-  ivLogsList?.forEach((log) => {
-    const logPath = `${logsPath}/${log.fileName}`;
-    if (!fs.existsSync(logPath)) {
-      logger.info('reports-handler: log file check, not exist');
-      return;
-    }
-
-    try {
-      fs.unlinkSync(logPath);
-      logger.info(`reports-handler: removed ${log.fileName}`);
-    } catch (e) {
-      logger.error(`reports-handler: Error in removing file - ${e}`);
-    }
-  });
-};
-
-const extractIVLogs: RetrieveIVLogs = (logsPath: string) => {
-  const ivFolderPath = path.join(
-    getHomedir(),
-    'AppData',
-    'Local',
-    'Cloud9_Technologies',
-    'C9Trader',
-    'log',
-  );
-
-  fileHelper.set(fileHelper.createFiles());
-  const ivLogsList = fileHelper.getLatestModifiedFiles(ivFolderPath);
-
-  if (ivLogsList && ivLogsList.size < 1) {
-    logger.info(`reports-handler: Cannot copy log, logs arent exist`);
-  } else {
-    ivLogsList?.forEach((log) => {
-      try {
-        logger.info(`reports-handler: Start reading logs, ${log.fileName}`);
-        const ivLog = fs.readFileSync(`${ivFolderPath}/${log.fileName}`);
-
-        if (ivLog) {
-          logger.info(`reports-handler: Log found start copying content`);
-          fs.writeFileSync(`${logsPath}/${log.fileName}`, ivLog);
-        } else {
-          logger.error(`reports-handler: ${log} cannot be found.`);
-        }
-      } catch (e) {
-        logger.error(`reports-handler: ${e}`);
-      }
-    });
-    fileHelper.unsetFiles();
-  }
 };
