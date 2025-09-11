79300794c22c6f7f9ea33b6d9662f0e02c9e7e01
Salah Benmoussati <salah.benmoussati@symphony.com>
2025-09-11 15:19:59 +0200

    Linux build fix

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -14,18 +14,12 @@ if [ -x "$(command -v nvm)" ]; then
   exit 1
 fi
 
-# # Source all the profile files to ensure nvm is in path
-# . $HOME/.nvm/nvm.sh
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# nvm --version
-
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="/data/nvm/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-                        
+
+
 # Verify that nvm is installed
 nvm --version
 
