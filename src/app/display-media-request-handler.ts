bef392cc411cd7de312b6d3ba93390cfd74fef23
Axel Eriksson <axel.eriksson@symphony.com>
2025-10-14 09:46:01 +0200

    Adding logs

--- src/app/display-media-request-handler.ts
+++ src/app/display-media-request-handler.ts
@@ -121,26 +121,32 @@ class DisplayMediaRequestHandler {
             // @ts-ignore
             callback();
           } else {
-            /**
-             * SDA-3646 hack for macOS: whenever we try to close the penultimate window
-             * (here screensharing screen picker), Electron activates the last Electron window.
-             *
-             * This behaviour was observed while trying to upgrade from Electron 14 to Electron 17
-             * Here the hack to solve that issue is to create a new invisible BrowserWindow.
-             */
-            this.screenPickerPlaceholderWindow = new BrowserWindow({
-              title: 'Screen sharing - Symphony',
-              width: 0,
-              height: 0,
-              transparent: true,
-              frame: false,
-              x: 0,
-              y: 0,
-              resizable: false,
-              movable: false,
-              fullscreenable: false,
-            });
-            this.screenPickerPlaceholderWindow.show();
+            try {
+              /**
+               * SDA-3646 hack for macOS: whenever we try to close the penultimate window
+               * (here screensharing screen picker), Electron activates the last Electron window.
+               *
+               * This behaviour was observed while trying to upgrade from Electron 14 to Electron 17
+               * Here the hack to solve that issue is to create a new invisible BrowserWindow.
+               */
+              this.screenPickerPlaceholderWindow = new BrowserWindow({
+                title: 'Screen sharing - Symphony',
+                width: 0,
+                height: 0,
+                transparent: true,
+                frame: false,
+                x: 0,
+                y: 0,
+                resizable: false,
+                movable: false,
+                fullscreenable: false,
+              });
+              this.screenPickerPlaceholderWindow.show();
+            } catch (err) {
+              logger.info('display-media-request-hander: Logging the error');
+              logger.info(JSON.stringify(err));
+            }
+
             callback({ video: source });
           }
         });
